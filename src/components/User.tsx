caffad48303024da825770721661ad2a81928603
CristianSifuentes <sifuentescovarrubiasitsl@gmail.com>
2025-05-14 18:14:58 -0600

    Add Counter and User component, using useState, strong typed and interfaces and other features

---
+++ src/components/User.tsx
@@ -0,0 +1,44 @@
+import { useState } from "react"
+import { Onclick } from '../../../GifsApp_/src/app/gifs/interfaces/giphy.interfaces';
+
+interface User {
+    uid:    string; 
+    name:   string;
+}
+
+const tempUser: User = {
+    uid: 'ABC123',  
+    name: 'Fernando'        
+}
+
+export const User = () => {
+
+  const [user, setUser] = useState<User>({
+    uid: '',
+    name: ''
+  });
+
+  const login = () => { 
+    setUser({
+      uid: 'ABC123',
+      name: 'Fernando'
+    });
+  }
+
+       
+  return (
+    <div className="mt-5">
+        <h3>User: useState</h3>
+        <button onClick={login} className="btn btn-outline-primary">login</button>
+
+        {
+            (!user)
+             ? 
+            <pre>Not found user:</pre> 
+            : <pre>{ JSON.stringify(user) }</pre>
+        }
+
+       
+    </div>
+  )
+}
