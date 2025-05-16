bf9d8ad02b26a5b95bc31ffc439e3be3b6685e5c
CristianSifuentes <sifuentescovarrubiasitsl@gmail.com>
2025-05-16 14:58:12 -0600

    Working with useEffect, Custom Props and useRef

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
 import { Counter } from './components/Counter';
+import { QuadraticController } from './components/QuadraticController';
+import { QuadraticPlot } from './components/QuadraticPlot';
 import { User } from './components/User';
+import { useState } from 'react';
 
 function App() {
   return (
     <div className="App">
       <h1>React + Typescript</h1>
       <hr></hr>
+
+      <h2>useState</h2>
       <Counter></Counter>
       <User></User>
 
+
+      <h2></h2>
+      <QuadraticController></QuadraticController>
+      <h2>Quadratic Plot</h2>
+      {/* <QuadraticPlot a={1} b={2} c={3}></QuadraticPlot> */}
     </div>
   );
 }
